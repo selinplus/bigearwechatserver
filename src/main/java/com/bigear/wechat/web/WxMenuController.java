af432a92acda4093bb701b8868b7f442f8f385e6
ytgs <ytgs@ytgss-iMac.local>
2018-06-20 09:30:18 +0800

    update

--- src/main/java/com/bigear/wechat/web/WxMenuController.java
+++ src/main/java/com/bigear/wechat/web/WxMenuController.java
@@ -49,7 +49,7 @@ public class WxMenuController implements WxMpMenuService {
     WxMenu menu = new WxMenu();
     WxMenuButton button1 = new WxMenuButton();
     button1.setName("机动车检测");
-    button1.setUrl("http://pf426y.natappfree.cc");
+    button1.setUrl("http://pitza5.natappfree.cc");
     button1.setType(MenuButtonType.VIEW);
     menu.getButtons().add(button1);
 //     WxMenuButton button1 = new WxMenuButton();
