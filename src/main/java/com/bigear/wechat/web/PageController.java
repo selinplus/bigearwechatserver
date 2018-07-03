db190694f96facd3ad04b917a9114ee7199c8f3b
ytgs <ytgs@ytgss-iMac.local>
2018-07-03 17:07:41 +0800

    version 0.1

--- src/main/java/com/bigear/wechat/web/PageController.java
+++ src/main/java/com/bigear/wechat/web/PageController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
   @RequestMapping("/")  
-    public String page(){  
-        return "index";  
-    }  
+  public String clientPage(){  
+      return "index";  
+  }  
+
+  @RequestMapping("/admin")  
+  public String serverPage(){  
+      return "server";  
+  }  
 }
